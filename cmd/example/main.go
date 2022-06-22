c4779585039ba084451aa1e2f2d2d7a0b4ef884b
Artem <bondarchuk.artem20@gmail.com>
2022-06-23 00:29:33 +0300

    fix: added closing of input file

--- cmd/example/main.go
+++ cmd/example/main.go
@@ -1,48 +1,50 @@
 package main
- 
+
 import (
- "flag"
- "fmt"
- "os"
- "strings"
- lab2 "github.com/Ararni/postfix-to-infix"
+	"flag"
+	"fmt"
+	"os"
+	"strings"
+
+	lab2 "github.com/Ararni/postfix-to-infix"
 )
- 
+
 var (
- inputExpression = flag.String("e", "", "Expression to compute")
- fileInput  = flag.String("f", "", "File containing expression to compute")
- fileOutput = flag.String("o", "", "Write expression to file")
- // TODO: Add other flags support for input and output configuration.
+	inputExpression = flag.String("e", "", "Expression to compute")
+	fileInput       = flag.String("f", "", "File containing expression to compute")
+	fileOutput      = flag.String("o", "", "Write expression to file")
+	// TODO: Add other flags support for input and output configuration.
 )
- 
+
 func main() {
- flag.Parse()
-  handler := &lab2.ComputeHandler{}
- if len(*fileInput) == 0{
-   handler.Input = strings.NewReader(*inputExpression)
- } else {
-   file, err := os.Open(*fileInput)
-   if err != nil {
-     fmt.Fprintln(os.Stderr, "error")
-     return
-   }
-   handler.Input = file
- }
- if len(*fileOutput) == 0{
-  handler.Output = os.Stdout
- } else {
-   file, err := os.OpenFile(*fileOutput, os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
-   if err != nil {
-     fmt.Fprintf(os.Stderr, "error")
-     return
-   }
-   handler.Output = file
- }
- 
- err := handler.Compute()
- if err != nil {
-   fmt.Fprintf(os.Stderr, err.Error())
-   return
- }
+	flag.Parse()
+	handler := &lab2.ComputeHandler{}
+	if len(*fileInput) == 0 {
+		handler.Input = strings.NewReader(*inputExpression)
+	} else {
+		file, err := os.Open(*fileInput)
+		if err != nil {
+			fmt.Fprintln(os.Stderr, "error")
+			return
+		}
+		defer file.Close()
+		handler.Input = file
+	}
+	if len(*fileOutput) == 0 {
+		handler.Output = os.Stdout
+	} else {
+		file, err := os.OpenFile(*fileOutput, os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "error")
+			return
+		}
+		defer file.Close()
+		handler.Output = file
+	}
+
+	err := handler.Compute()
+	if err != nil {
+		fmt.Fprintf(os.Stderr, err.Error())
+		return
+	}
 }
- 
